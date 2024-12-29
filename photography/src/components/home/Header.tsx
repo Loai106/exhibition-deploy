6d43ec1d00fe6676cd28f46a7f8ff8f34583997d
Loai Mekbel <loai@192.168.1.79>
2024-12-29 13:11:28 +0300

    fix some issues the images

--- photography/src/components/home/Header.tsx
+++ photography/src/components/home/Header.tsx
@@ -2,33 +2,33 @@ import React from "react";
 import { Box, Button } from "@mui/material";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import img1 from "/images/carousel1.jpeg";
+//import img1 from "/images/carousel1.jpeg";
 import img2 from "/images/carousel2.jpeg";
-import img3 from "/images/carousel3.jpeg";
-import img4 from "/images/carousel4.jpeg";
+//import img3 from "/images/carousel3.jpeg";
+//import img4 from "/images/carousel4.jpeg";
 
 const Header: React.FC = () => {
   const images = [
-    {
-      url: img4,
-      text: "Explore Stunning Photography",
-      description: "Discover captivating moments captured through our lens.",
-    },
-    {
-      url: img3,
-      text: "Unveil Unique Perspectives",
-      description: "Experience the art of storytelling through photography.",
-    },
+    // {
+    //   url: img4,
+    //   text: "Explore Stunning Photography",
+    //   description: "Discover captivating moments captured through our lens.",
+    // },
+    // {
+    //   url: img3,
+    //   text: "Unveil Unique Perspectives",
+    //   description: "Experience the art of storytelling through photography.",
+    // },
     {
       url: img2,
       text: "Embrace Vibrant Colors",
       description: "Dive into the beauty and creativity of vivid imagery.",
     },
-    {
-      url: img1,
-      text: "Embrace Vibrant Colors",
-      description: "Dive into the beauty and creativity of vivid imagery.",
-    },
+    // {
+    //   url: img1,
+    //   text: "Embrace Vibrant Colors",
+    //   description: "Dive into the beauty and creativity of vivid imagery.",
+    // },
   ];
   return (
     <Carousel
